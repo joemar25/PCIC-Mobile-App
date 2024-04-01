f878faec15ff35f9ef2e295776622958a5368fad
JCBEA <111287289+JCBEA@users.noreply.github.com>
2024-04-01 17:08:28 +0800

    added table

--- lib/screens/dashboard/_settings.dart
+++ lib/screens/dashboard/_settings.dart
@@ -1,21 +1,46 @@
 import 'package:flutter/material.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
 
   @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  final List<Map<String, dynamic>> _data = [
+    {'taskID': '1231123', 'jobID': '123 Main St', 'isComplete': 'june 1, 2022', 'dateAdded': 'june 1, 2022', 'dateAccess': 'june 1, 2022'},
+    {'taskID': '213123w', 'jobID': '123 Main St', 'isComplete': 'june 1, 2022', 'dateAdded': 'june 1, 2022', 'dateAccess': 'june 1, 2022'},
+    {'taskID': '11d1', 'jobID': '123 Main St', 'isComplete': 'june 1, 2022', 'dateAdded': 'june 1, 2022', 'dateAccess': 'june 1, 2022'},
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('User Information'),
+      ),
       body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Center(
-          child: Text(
-            'Settings',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
+        padding: const EdgeInsets.all(16.0),
+        child: DataTable(
+          columns: const [
+            DataColumn(label: Text('taskID')),
+            DataColumn(label: Text('jobID')),
+            DataColumn(label: Text('isComplete')),
+            DataColumn(label: Text('dateAdded')),
+             DataColumn(label: Text('dateAccess')),
+          ],
+          rows: _data.map((entry) {
+            return DataRow(
+              cells: [
+                DataCell(Text(entry['taskID'].toString())),
+                DataCell(Text(entry['jobID'].toString())),
+                DataCell(Text(entry['isComplete'].toString())),
+                DataCell(Text(entry['dateAdded'].toString())),
+                DataCell(Text(entry['dateAccess'].toString())),
+              ],
+            );
+          }).toList(),
         ),
       ),
     );
