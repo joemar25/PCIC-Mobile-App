9663a490066e8b768ee856f56ea496ecd87ec04b
joemar25 <joemar.cardino000@gmail.com>
2024-04-02 11:06:10 +0800

    Add flutter_map_cancellable_tile_provider dependency

--- lib/screens/dashboard/views/_geotag.dart
+++ lib/screens/dashboard/views/_geotag.dart
@@ -4,6 +4,7 @@ import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:pcic_mobile_app/screens/dashboard/controllers/_control_task.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter_map_cancellable_tile_provider/flutter_map_cancellable_tile_provider.dart';
 
 class GeotagPage extends StatefulWidget {
   final Task task;
@@ -17,6 +18,9 @@ class _GeotagPageState extends State<GeotagPage> {
   String currentLocation = '';
   List<Marker> markers = [];
   MapController mapController = MapController();
+  bool isColumnVisible = true;
+  bool isPinDropMode = false;
+  bool isRoutingStarted = false;
 
   @override
   void initState() {
@@ -41,18 +45,10 @@ class _GeotagPageState extends State<GeotagPage> {
       setState(() {
         currentLocation =
             'Lat: ${position.latitude}, Long: ${position.longitude}';
-        markers.add(
-          Marker(
-            width: 80.0,
-            height: 80.0,
-            point: LatLng(position.latitude, position.longitude),
-            child: const Icon(
-              // Change this line
-              Icons.location_on,
-              color: Colors.blue,
-              size: 40.0,
-            ),
-          ),
+        _addMarker(LatLng(position.latitude, position.longitude));
+        mapController.move(
+          LatLng(position.latitude, position.longitude),
+          15.0,
         );
       });
     } catch (e) {
@@ -60,29 +56,168 @@ class _GeotagPageState extends State<GeotagPage> {
     }
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Geotagging'),
-      ),
-      body: FlutterMap(
-        mapController: mapController,
-        options: const MapOptions(
-          center: LatLng(13.138769, 123.734005),
-          zoom: 13.0,
-        ),
-        children: [
-          TileLayer(
-            urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-            subdomains: const ['a', 'b', 'c'],
+  void _addMarker(LatLng point) {
+    setState(() {
+      markers.add(
+        Marker(
+          width: 80.0,
+          height: 80.0,
+          point: point,
+          child: const Icon(
+            // Change this line
+            Icons.location_on,
+            color: Colors.blue,
+            size: 40.0,
           ),
-          MarkerLayer(markers: markers),
+        ),
+      );
+    });
+  }
+
+  void _startRouting() {
+    debugPrint('Starting');
+    setState(() {
+      isRoutingStarted = true;
+    });
+    _getCurrentLocation();
+  }
+
+  void _stopRouting() {
+    setState(() {
+      isRoutingStarted = false;
+      isPinDropMode = false;
+    });
+    // Add your logic for stopping the routing here
+  }
+
+  void _togglePinDropMode() {
+    setState(() {
+      isPinDropMode = !isPinDropMode;
+    });
+  }
+
+  Future<bool> _onWillPop() async {
+    final shouldPop = await showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Confirmation'),
+        content: const Text('Are you sure you want to cancel?'),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(false),
+            child: const Text('No'),
+          ),
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(true),
+            child: const Text('Yes'),
+          ),
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _getCurrentLocation,
-        child: const Icon(Icons.my_location),
+    );
+    return shouldPop ?? false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Geotag'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: FlutterMap(
+                mapController: mapController,
+                options: MapOptions(
+                  initialCenter: const LatLng(13.138769, 123.734005),
+                  initialZoom: 18.0, // Increase the initial zoom level
+                  maxZoom: 22.0, // Set the maximum zoom level
+                  onTap: (tapPosition, point) {
+                    if (isPinDropMode && isRoutingStarted) {
+                      _addMarker(point);
+                    }
+                  },
+                ),
+                children: [
+                  TileLayer(
+                    urlTemplate:
+                        'https://api.mapbox.com/styles/v1/quanbysolutions/cluhoxol502q801oi8od2cmvz/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicXVhbmJ5c29sdXRpb25zIiwiYSI6ImNsdWhrejRwdDJyYnAya3A2NHFqbXlsbHEifQ.WJ5Ng-AO-dTrlkUHD_ebMw',
+                    additionalOptions: const {
+                      'accessToken':
+                          'pk.eyJ1IjoicXVhbmJ5c29sdXRpb25zIiwiYSI6ImNsdWhrejRwdDJyYnAya3A2NHFqbXlsbHEifQ.WJ5Ng-AO-dTrlkUHD_ebMw',
+                      'id': 'mapbox.satellite',
+                    },
+                    tileProvider: CancellableNetworkTileProvider(),
+                  ),
+                  MarkerLayer(markers: markers),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Current Location',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 8),
+                  Text(currentLocation),
+                  const SizedBox(height: 20),
+                  Visibility(
+                    visible: isColumnVisible,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Route Points',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        const SizedBox(height: 20),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            ElevatedButton(
+                              onPressed: _startRouting,
+                              child: const Text('Start Routing'),
+                            ),
+                            ElevatedButton(
+                              onPressed: _stopRouting,
+                              child: const Text('Stop Routing'),
+                            ),
+                            Visibility(
+                              visible: isRoutingStarted,
+                              child: ElevatedButton(
+                                onPressed: _togglePinDropMode,
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: isPinDropMode
+                                      ? Colors.green
+                                      : Colors.blue,
+                                ),
+                                child: const Text('Pin Drop'),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _getCurrentLocation,
+          child: const Icon(Icons.my_location),
+        ),
       ),
     );
   }
