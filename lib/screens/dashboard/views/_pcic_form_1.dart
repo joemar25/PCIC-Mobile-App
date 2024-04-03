1ba52b6426415695f8c6bfc186e4e46e6260a193
joemar25 <joemar.cardino000@gmail.com>
2024-04-03 16:45:16 +0800

    Refactor import paths and add camera dependency

--- lib/screens/dashboard/views/_pcic_form_1.dart
+++ lib/screens/dashboard/views/_pcic_form_1.dart
@@ -1,11 +1,72 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:pcic_mobile_app/utils/controls/_control_task.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:open_file/open_file.dart';
+import 'package:path_provider/path_provider.dart';
 
-class PCICFormPage extends StatelessWidget {
-  final Task task;
+class PCICFormPage extends StatefulWidget {
+  final String imageFile;
   final String gpxFile;
 
-  const PCICFormPage({super.key, required this.task, required this.gpxFile});
+  const PCICFormPage(
+      {super.key, required this.imageFile, required this.gpxFile});
+
+  @override
+  _PCICFormPageState createState() => _PCICFormPageState();
+}
+
+class _PCICFormPageState extends State<PCICFormPage> {
+  File? _confirmedBySignature;
+  File? _preparedBySignature;
+
+  Future<void> _pickImage(bool isConfirmedBy) async {
+    final picker = ImagePicker();
+    final pickedImage = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedImage != null) {
+      setState(() {
+        if (isConfirmedBy) {
+          _confirmedBySignature = File(pickedImage.path);
+        } else {
+          _preparedBySignature = File(pickedImage.path);
+        }
+      });
+    }
+  }
+
+  void _downloadGpxFile(String gpxFilePath) async {
+    try {
+      final externalStorageDirectory = await getExternalStorageDirectory();
+      const gpxFileName = 'route.gpx';
+      final gpxFilePath = '${externalStorageDirectory!.path}/$gpxFileName';
+
+      await FlutterDownloader.enqueue(
+        url: 'file://$gpxFilePath',
+        savedDir: externalStorageDirectory.path,
+        fileName: gpxFileName,
+        showNotification: true,
+        openFileFromNotification: true,
+      );
+    } catch (e) {
+      print('Error downloading GPX file: $e');
+    }
+  }
+
+  void _openGpxFile(String gpxFilePath) async {
+    try {
+      final externalStorageDirectory = await getExternalStorageDirectory();
+      final gpxFilePath = '${externalStorageDirectory!.path}/route.gpx';
+
+      if (await File(gpxFilePath).exists()) {
+        await OpenFile.open(gpxFilePath);
+      } else {
+        print('GPX file not found: $gpxFilePath');
+      }
+    } catch (e) {
+      print('Error opening GPX file: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,51 +78,90 @@ class PCICFormPage extends StatelessWidget {
         padding: const EdgeInsets.all(16.0),
         child: ListView(
           children: <Widget>[
-            // TextFormField(
-            //   initialValue: task.title,
-            //   decoration: const InputDecoration(labelText: 'Title'),
-            //   enabled: false,
-            // ),
-            // TextFormField(
-            //   initialValue: task.description,
-            //   decoration: const InputDecoration(labelText: 'Description'),
-            //   enabled: false,
-            // ),
             TextFormField(
-              initialValue: task.formData['companyName'] ?? '',
-              decoration: const InputDecoration(labelText: 'Company Name'),
-              enabled: false,
+              initialValue: '',
+              decoration: const InputDecoration(labelText: 'Area Planted*'),
+              enabled: true,
             ),
             TextFormField(
-              initialValue: task.formData['registrationNumber'] ?? '',
+              initialValue: '',
               decoration:
-                  const InputDecoration(labelText: 'Registration Number'),
-              enabled: false,
+                  const InputDecoration(labelText: 'Date of Planting (DS)*'),
+              enabled: true,
             ),
             TextFormField(
-              initialValue: task.formData['ownerName'] ?? '',
-              decoration: const InputDecoration(labelText: 'Owner Name'),
-              enabled: false,
+              initialValue: '',
+              decoration:
+                  const InputDecoration(labelText: 'Date of Planting (TP)*'),
+              enabled: true,
             ),
             TextFormField(
-              initialValue: task.formData['email'] ?? '',
-              decoration: const InputDecoration(labelText: 'Email'),
-              enabled: false,
+              initialValue: '',
+              decoration:
+                  const InputDecoration(labelText: 'Seed Variety Planted*'),
+              enabled: true,
+            ),
+            TextFormField(
+              initialValue: '',
+              decoration: const InputDecoration(labelText: 'Remarks'),
+              enabled: true,
             ),
             const SizedBox(height: 20),
             const Text(
-              'Signature Area',
+              'Confirmed by:',
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
-            Container(
-              height: 200,
-              color: Colors.grey[200],
-              child: const Center(
-                child: Text(
-                  'Signature Placeholder',
-                  style: TextStyle(fontSize: 18),
-                ),
+            GestureDetector(
+              onTap: () => _pickImage(true),
+              child: Container(
+                height: 200,
+                color: Colors.grey[200],
+                child: _confirmedBySignature != null
+                    ? Image.file(
+                        _confirmedBySignature!,
+                        fit: BoxFit.cover,
+                      )
+                    : const Center(
+                        child: Text(
+                          'Tap to add signature',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
               ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Prepared by:',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            GestureDetector(
+              onTap: () => _pickImage(false),
+              child: Container(
+                height: 200,
+                color: Colors.grey[200],
+                child: _preparedBySignature != null
+                    ? Image.file(
+                        _preparedBySignature!,
+                        fit: BoxFit.cover,
+                      )
+                    : const Center(
+                        child: Text(
+                          'Tap to add signature',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Map Screenshot',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            Image.file(
+              File(widget.imageFile),
+              height: 200,
+              fit: BoxFit.cover,
             ),
             const SizedBox(height: 20),
             const Text(
@@ -69,9 +169,16 @@ class PCICFormPage extends StatelessWidget {
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 10),
-            Text(
-              gpxFile,
-              style: const TextStyle(fontSize: 16),
+            ElevatedButton(
+              onPressed: () => _openGpxFile(widget.gpxFile),
+              child: const Text('Open GPX File'),
+            ),
+            ElevatedButton(
+              onPressed: () => '',
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.green),
+              ),
+              child: const Text('Finish'),
             ),
           ],
         ),
