886b2ec31b7b17eba4671884ef547218f5dabf78
Scott Andrew Bedis <bedisscottandrew@gmail.com>
2024-05-07 18:38:15 +0800

    pcic_form: added flash message

--- lib/utils/app/_show_flash_message.dart
+++ lib/utils/app/_show_flash_message.dart
@@ -1,35 +1,48 @@
 import 'package:flash/flash.dart';
 import 'package:flash/flash_helper.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart'; // Import the flutter_svg package
 
 void showFlashMessage(
     BuildContext context, String status, String title, String content) {
+  Color statusColor;
+  IconData statusIcon;
+
+  switch (status) {
+    case 'Success':
+      statusColor = Colors.green.shade300;
+      statusIcon = Icons.check_circle;
+      break;
+    case 'Error':
+      statusColor = Colors.red.shade300;
+      statusIcon = Icons.error;
+      break;
+    case 'Info':
+      statusColor = Colors.blue.shade300;
+      statusIcon = Icons.info;
+      break;
+    default:
+      statusColor = Colors.grey.shade300;
+      statusIcon = Icons.help_outline;
+  }
   context.showFlash(
-    duration: const Duration(seconds: 3),
+    duration: const Duration(seconds: 4),
     builder: (context, controller) => FlashBar(
       controller: controller,
       behavior: FlashBehavior.floating,
-      shape: RoundedRectangleBorder(
-        borderRadius: const BorderRadius.all(Radius.circular(16)),
-        side: BorderSide(
-          color: status == 'Success' ? Colors.green : Colors.red,
-          strokeAlign: BorderSide.strokeAlignInside,
-        ),
+      shadowColor: statusColor,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(16)),
       ),
+      backgroundColor: Colors.white,
       surfaceTintColor: Colors.white,
+
       margin: const EdgeInsets.all(32.0),
       clipBehavior: Clip.antiAlias,
-      indicatorColor: status == 'Success' ? Colors.green : Colors.red,
-      icon: SvgPicture.asset(
-        status == 'Success'
-            ? 'assets/storage/images/success.svg'
-            : 'assets/storage/images/error.svg',
-        height: 35,
-        colorFilter: ColorFilter.mode(
-            status == 'Success' ? Colors.green.shade900 : Colors.red.shade900,
-            BlendMode.srcIn),
-      ),
+      shouldIconPulse: false,
+
+      // indicatorColor:
+      icon: Icon(statusIcon, size: 32, color: statusColor),
+
       title: Text(
         title,
         style: const TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold),
