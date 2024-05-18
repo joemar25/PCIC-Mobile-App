d6a099a6e2c1bb49ff5397cc8434ba67745dc0a1
cleomark <cleocalitisin@gmail.com>
2024-05-18 14:54:59 +0800

    add a subtle shadow on search box

--- lib/src/home/_search_button.dart
+++ lib/src/home/_search_button.dart
@@ -1,9 +1,11 @@
 // filename: _search_button.dart
+
 import 'package:flutter/material.dart';
 import 'package:pcic_mobile_app/src/theme/_theme.dart';
 
 class SearchButton extends StatefulWidget {
   final ValueChanged<String> onUpdateValue;
+
   const SearchButton({super.key, required this.onUpdateValue});
 
   @override
@@ -15,32 +17,47 @@ class _SearchButtonState extends State<SearchButton> {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 50,
-      child: Expanded(
-        child: TextField(
-          onChanged: widget.onUpdateValue,
-          textAlign: TextAlign.left, // Text aligns to the left
-          decoration: InputDecoration(
-            hintText: 'Search Task',
-            hintStyle: const TextStyle(color: mainColor),
-            isDense: true,
-            contentPadding:
-                const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-            focusedBorder: OutlineInputBorder(
-              borderSide: const BorderSide(color: mainColor, width: 1.0),
-              borderRadius: BorderRadius.circular(32.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(32.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              spreadRadius: 1,
+              blurRadius: 5,
+              offset: const Offset(0, 3),
             ),
-            enabledBorder: OutlineInputBorder(
-              borderSide: const BorderSide(color: mainColor, width: 1.0),
-              borderRadius: BorderRadius.circular(32.0),
-            ),
-            suffixIconConstraints:
-                const BoxConstraints(minWidth: 50, minHeight: 50),
-            suffixIcon: const Padding(
-              padding: EdgeInsetsDirectional.only(end: 12.0),
-              child: Icon(
-                Icons.search,
-                color: mainColor,
-                size: 30,
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(32.0),
+          child: TextField(
+            onChanged: widget.onUpdateValue,
+            textAlign: TextAlign.left,
+            decoration: InputDecoration(
+              hintText: 'Search Task',
+              hintStyle: const TextStyle(color: mainColor),
+              isDense: true,
+              contentPadding:
+                  const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+              focusedBorder: OutlineInputBorder(
+                borderSide: const BorderSide(color: mainColor, width: 1.0),
+                borderRadius: BorderRadius.circular(32.0),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderSide: const BorderSide(color: mainColor, width: 1.0),
+                borderRadius: BorderRadius.circular(32.0),
+              ),
+              suffixIconConstraints:
+                  const BoxConstraints(minWidth: 50, minHeight: 50),
+              suffixIcon: const Padding(
+                padding: EdgeInsetsDirectional.only(end: 12.0),
+                child: Icon(
+                  Icons.search,
+                  color: mainColor,
+                  size: 30,
+                ),
               ),
             ),
           ),
