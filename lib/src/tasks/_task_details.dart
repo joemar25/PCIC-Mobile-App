62e45e8fc1831d29fafcf0979661ac673dc90f5a
cleomark <cleocalitisin@gmail.com>
2024-05-21 12:40:19 +0800

    improvetask details ui

--- lib/src/tasks/_task_details.dart
+++ lib/src/tasks/_task_details.dart
@@ -33,87 +33,42 @@ class TaskDetailsPage extends StatelessWidget {
     return serviceCode == null ? '' : romanMap[serviceCode] ?? serviceCode;
   }
 
-  Widget _buildFormSection(
-      BuildContext context, String title, List<Widget> fields) {
-    final t = Theme.of(context).extension<CustomThemeExtension>();
-    return AnimatedSize(
-      duration: const Duration(milliseconds: 500),
-      curve: Curves.easeInOut,
-      child: SizedBox(
-        width: double.infinity,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              padding: const EdgeInsets.only(bottom: 12.0),
-              alignment: Alignment.center,
-              child: Text(title,
-                  style: TextStyle(
-                    fontSize: t?.headline,
-                    fontWeight: FontWeight.bold,
-                  )),
-            ),
-            Container(
-              width: double.maxFinite,
-              margin: const EdgeInsets.only(bottom: 16.0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(width: 0.2, color: Colors.grey),
-                boxShadow: [
-                  // BoxShadow(color: Color(0xFF0F7D40), offset: Offset(-5, 5))
-                  BoxShadow(
-                    color: const Color(0xFF0F7D40).withOpacity(0.8),
-                    blurRadius: 1,
-                    spreadRadius: 1,
-                    offset: const Offset(0, 3),
-                  ),
-                ],
-                // boxShadow: [
-                //   BoxShadow(
-                //     color: Colors.black.withOpacity(0.2),
-                //     spreadRadius: 2,
-                //     blurRadius: 2,
-                //     offset: const Offset(0, 5),
-                //   )
-                // ],
-                borderRadius: BorderRadius.circular(15.0),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    const SizedBox(height: 18.0),
-                    ...fields,
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
   Widget _buildFormField(BuildContext context, String label, String? value) {
     final t = Theme.of(context).extension<CustomThemeExtension>();
     final displayValue = value?.isNotEmpty ?? false ? value : 'N/A';
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 2.0),
+      padding: const EdgeInsets.symmetric(vertical: 12.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('$label:',
-              style: TextStyle(
-                color: const Color(0xFF0F7D40),
-                fontWeight: FontWeight.w600,
-                fontSize: t?.caption ?? 14.0,
-              )),
-          Padding(
-            padding: const EdgeInsets.only(bottom: 4.0),
-            child: Text(displayValue!,
-                style:
-                    TextStyle(fontSize: t?.body, fontWeight: FontWeight.w600)),
+          Text(
+            '$label:',
+            style: TextStyle(
+              color: const Color(0xFF0F7D40),
+              fontWeight: FontWeight.w600,
+              fontSize: t?.caption ?? 14.0,
+            ),
+          ),
+          const SizedBox(height: 4.0),
+          Container(
+            width: double.infinity,
+            padding:
+                const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(8.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.1),
+                  blurRadius: 4,
+                  offset: const Offset(0, 2),
+                ),
+              ],
+            ),
+            child: Text(
+              displayValue!,
+              style: TextStyle(fontSize: t?.body, fontWeight: FontWeight.w600),
+            ),
           ),
         ],
       ),
@@ -175,134 +130,174 @@ class TaskDetailsPage extends StatelessWidget {
               return SingleChildScrollView(
                 padding: const EdgeInsets.all(16.0),
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     if (status != 'Completed')
                       Padding(
-                        padding: const EdgeInsets.only(bottom: 16.0),
-                        child: Column(
-                          children: [
-                            ElevatedButton(
-                              onPressed: () => _navigateToGeotagPage(context),
-                              style: ElevatedButton.styleFrom(
-                                minimumSize: const Size(0, 60),
-                                shape: const CircleBorder(),
-                                backgroundColor: const Color(0xFF0F7D40),
+                        padding: const EdgeInsets.only(bottom: 24.0),
+                        child: Center(
+                          child: ElevatedButton(
+                            onPressed: () => _navigateToGeotagPage(context),
+                            style: ElevatedButton.styleFrom(
+                              minimumSize: const Size(200, 50),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(25.0),
                               ),
-                              child: SizedBox(
-                                height: 35,
-                                width: 35,
-                                child: SvgPicture.asset(
+                              backgroundColor: const Color(0xFF0F7D40),
+                            ),
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                SvgPicture.asset(
                                   'assets/storage/images/geotag.svg',
+                                  width: 24,
+                                  height: 24,
                                   colorFilter: const ColorFilter.mode(
-                                      Colors.white, BlendMode.srcIn),
+                                    Colors.white,
+                                    BlendMode.srcIn,
+                                  ),
                                 ),
-                              ),
-                            ),
-                            Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(vertical: 8.0),
-                              child: Text(
-                                'Go to Geotag',
-                                style: TextStyle(
-                                  fontSize: t?.body,
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.w600,
+                                const SizedBox(width: 8.0),
+                                Text(
+                                  'Go to Geotag',
+                                  style: TextStyle(
+                                    fontSize: t?.body,
+                                    fontWeight: FontWeight.w600,
+                                    color: Colors.white,
+                                  ),
                                 ),
-                              ),
+                              ],
                             ),
-                          ],
+                          ),
                         ),
                       ),
-                    _buildFormSection(
-                        context, 'Post Planting Inspection Report', [
-                      _buildFormField(
-                          context, 'Farmer Name', formData['ppirFarmerName']),
-                      _buildFormField(
-                          context, 'Address', formData['ppirAddress']),
-                      _buildFormField(
-                          context, 'Mobile No.', formData['ppirMobileNo']),
-                      _buildFormField(context, 'Type of Farmers',
-                          formData['ppirFarmerType']),
-                      const Padding(
-                        padding: EdgeInsets.symmetric(vertical: 4.0),
-                        child: Divider(),
+                    Text(
+                      'Post Planting Inspection Report',
+                      style: TextStyle(
+                        fontSize: t?.headline,
+                        fontWeight: FontWeight.bold,
                       ),
-                      _buildFormField(
-                          context, 'Group Name', formData['ppirGroupName']),
-                      _buildFormField(context, 'Group Address',
-                          formData['ppirGroupAddress']),
-                      const Padding(
-                        padding: EdgeInsets.symmetric(vertical: 4.0),
-                        child: Divider(),
+                    ),
+                    const SizedBox(height: 16.0),
+                    _buildFormField(
+                        context, 'Farmer Name', formData['ppirFarmerName']),
+                    _buildFormField(
+                        context, 'Address', formData['ppirAddress']),
+                    _buildFormField(
+                        context, 'Mobile No.', formData['ppirMobileNo']),
+                    _buildFormField(
+                        context, 'Type of Farmers', formData['ppirFarmerType']),
+                    const Divider(height: 32.0),
+                    _buildFormField(
+                        context, 'Group Name', formData['ppirGroupName']),
+                    _buildFormField(
+                        context, 'Group Address', formData['ppirGroupAddress']),
+                    const Divider(height: 32.0),
+                    _buildFormField(
+                        context, 'Lender Name', formData['ppirLenderName']),
+                    _buildFormField(context, 'Lender Address',
+                        formData['ppirLenderAddress']),
+                    const Divider(height: 32.0),
+                    _buildFormField(
+                      context,
+                      'Region',
+                      _convertServiceGroupToRoman(formData['serviceGroup']),
+                    ),
+                    _buildFormField(
+                        context, 'Location of Farm', formData['ppirFarmLoc']),
+                    _buildFormField(context, 'CIC No.', formData['ppirCicNo']),
+                    const SizedBox(height: 24.0),
+                    Text(
+                      'Location Sketch Plan',
+                      style: TextStyle(
+                        fontSize: t?.headline,
+                        fontWeight: FontWeight.bold,
                       ),
-                      _buildFormField(
-                          context, 'Lender Name', formData['ppirLenderName']),
-                      _buildFormField(context, 'Lender Address',
-                          formData['ppirLenderAddress']),
-                      const Padding(
-                        padding: EdgeInsets.symmetric(vertical: 4.0),
-                        child: Divider(),
+                    ),
+                    const SizedBox(height: 16.0),
+                    _buildFormField(context, 'North', formData['ppirNorth']),
+                    _buildFormField(context, 'East', formData['ppirEast']),
+                    _buildFormField(context, 'South', formData['ppirSouth']),
+                    _buildFormField(context, 'West', formData['ppirWest']),
+                    const SizedBox(height: 24.0),
+                    Text(
+                      'Findings (Per ACI)',
+                      style: TextStyle(
+                        fontSize: t?.headline,
+                        fontWeight: FontWeight.bold,
                       ),
-                      _buildFormField(
-                          context,
-                          'Region',
-                          _convertServiceGroupToRoman(
-                              formData['serviceGroup'])),
-                      _buildFormField(
-                          context, 'Location of Farm', formData['ppirFarmLoc']),
-                      _buildFormField(
-                          context, 'CIC No.', formData['ppirCicNo']),
-                    ]),
-                    _buildFormSection(context, 'Location Sketch Plan', [
-                      _buildFormField(context, 'North', formData['ppirNorth']),
-                      _buildFormField(context, 'East', formData['ppirEast']),
-                      _buildFormField(context, 'South', formData['ppirSouth']),
-                      _buildFormField(context, 'West', formData['ppirWest']),
-                    ]),
-                    _buildFormSection(context, 'Findings (Per ACI)', [
-                      _buildFormField(
-                          context, 'Area Planted', formData['ppirAreaAci']),
-                      _buildFormField(context, 'Date of Planting (DS)',
-                          formData['ppirDopdsAci']),
-                      _buildFormField(context, 'Date of Planting (TP)',
-                          formData['ppirDoptpAci']),
-                      _buildFormField(context, 'Seed Variety Planted',
-                          formData['ppirVariety']),
-                    ]),
+                    ),
+                    const SizedBox(height: 16.0),
+                    _buildFormField(
+                        context, 'Area Planted', formData['ppirAreaAci']),
+                    _buildFormField(context, 'Date of Planting (DS)',
+                        formData['ppirDopdsAci']),
+                    _buildFormField(context, 'Date of Planting (TP)',
+                        formData['ppirDoptpAci']),
+                    _buildFormField(context, 'Seed Variety Planted',
+                        formData['ppirVariety']),
                     if (status == 'Completed') ...[
-                      _buildFormSection(context, 'Tracking Results', [
-                        _buildFormField(context, 'Last Coordinates',
-                            formData['trackLastcoord']),
-                        _buildFormField(
-                            context,
-                            'Date and Time',
-                            formData['trackDatetime'] != null
-                                ? DateTime.parse(formData['trackDatetime'])
-                                    .toString()
-                                : ''),
-                        _buildFormField(context, 'Total Area (Hectares)',
-                            formData['trackTotalarea']),
-                        _buildFormField(context, 'Total Distance',
-                            formData['trackTotaldistance']),
-                      ]),
-                      _buildFormSection(context, 'Actual Details', [
-                        _buildFormField(context, 'Last Coordinates',
-                            formData['ppirAreaAct']),
-                        _buildFormField(
-                            context, 'Seed Variety', formData['ppirVariety']),
-                        _buildFormField(context, 'Actual Date of Planting (DS)',
-                            formData['ppirDopdsAct']),
-                        _buildFormField(context, 'Actual Date of Planting (TP)',
-                            formData['ppirDoptpAct']),
-                        _buildFormField(
-                            context, 'Remarks', formData['ppirRemarks']),
-                      ]),
-                      _buildFormSection(context, 'Assignees', [
-                        _buildFormField(context, 'Confirmed By',
-                            formData['ppirNameInsured']),
-                        _buildFormField(
-                            context, 'Prepared By', formData['ppirNameIuia']),
-                      ]),
+                      const SizedBox(height: 24.0),
+                      Text(
+                        'Tracking Results',
+                        style: TextStyle(
+                          fontSize: t?.headline,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 16.0),
+                      _buildFormField(context, 'Last Coordinates',
+                          formData['trackLastcoord']),
+                      _buildFormField(
+                        context,
+                        'Date and Time',
+                        formData['trackDatetime'] != null
+                            ? DateTime.parse(formData['trackDatetime'])
+                                .toString()
+                            : '',
+                      ),
+                      _buildFormField(context, 'Total Area (Hectares)',
+                          formData['trackTotalarea']),
+                      _buildFormField(context, 'Total Distance',
+                          formData['trackTotaldistance']),
+                      const SizedBox(height: 24.0),
+                      Text(
+                        'Actual Details',
+                        style: TextStyle(
+                          fontSize: t?.headline,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 16.0),
+                      _buildFormField(
+                          context, 'Last Coordinates', formData['ppirAreaAct']),
+                      _buildFormField(
+                          context, 'Seed Variety', formData['ppirVariety']),
+                      _buildFormField(
+                        context,
+                        'Actual Date of Planting (DS)',
+                        formData['ppirDopdsAct'],
+                      ),
+                      _buildFormField(
+                        context,
+                        'Actual Date of Planting (TP)',
+                        formData['ppirDoptpAct'],
+                      ),
+                      _buildFormField(
+                          context, 'Remarks', formData['ppirRemarks']),
+                      const SizedBox(height: 24.0),
+                      Text(
+                        'Assignees',
+                        style: TextStyle(
+                          fontSize: t?.headline,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 16.0),
+                      _buildFormField(
+                          context, 'Confirmed By', formData['ppirNameInsured']),
+                      _buildFormField(
+                          context, 'Prepared By', formData['ppirNameIuia']),
                     ],
                   ],
                 ),
